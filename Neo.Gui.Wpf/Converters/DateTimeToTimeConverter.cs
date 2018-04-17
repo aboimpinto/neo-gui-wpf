6a6e39a6e889678e0054fcdb8b5e45c7f5a3bd7e
Paulo Aboim Pinto <aboimpinto@gmail.com>
2018-04-17 23:37:17 +0200

    Showing date and time of the transaction in different lines.

---
+++ Neo.Gui.Wpf/Converters/DateTimeToTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Neo.Gui.Wpf.Converters
+{
+    public class DateTimeToTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            var dateTime = DateTime.Parse(value.ToString());
+
+            return $"{dateTime.Hour:00}:{dateTime.Minute:00}:{dateTime.Second:00}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Neo.Gui.Wpf/Converters/DatetimeToDateConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Neo.Gui.Wpf.Converters
+{
+    public class DatetimeToDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            var dateTime = DateTime.Parse(value.ToString());
+
+            return $"{dateTime.Year:00}.{dateTime.Month:00}.{dateTime.Day:00}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
