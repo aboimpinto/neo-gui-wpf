4b7238fefa0d9fefda5db2286eceece008553009
Paulo Aboim Pinto <aboimpinto@gmail.com>
2018-04-29 14:54:31 +0200

    First draft of MangeAccountsView

---
+++ Neo.Gui.ViewModels/ScreenViewModels/ManageAccountsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Neo.UI.Core.Messaging.Interfaces;
+
+namespace Neo.Gui.ViewModels.ScreenViewModels
+{
+    public class ManageAccountsViewModel : ViewModelBase
+    {
+        #region Private fields
+        private readonly IMessagePublisher _messagePublisher;
+        #endregion
+
+        #region Public Properties 
+        public RelayCommand BackCommand { get; set; }
+        #endregion
+
+        #region Constructor
+        public ManageAccountsViewModel(IMessagePublisher messagePublisher)
+        {
+            this._messagePublisher = messagePublisher;
+
+            this.BackCommand = new RelayCommand(this.HandleBackNaviation);
+        }
+        #endregion
+
+        #region Private Methods 
+        private void HandleBackNaviation()
+        {
+            this._messagePublisher.Publish(new NavigationMessage(ViewNames.DashboardView));
+        }
+        #endregion
+    }
+}
